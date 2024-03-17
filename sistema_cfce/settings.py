a5f771f126f70f452f15ffa4bdd468193f46542a
unknown <josercepeda01@gmail.com>
2024-03-17 12:21:38 -0500

    set locale

--- sistema_cfce/settings.py
+++ sistema_cfce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import locale
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-Co'
 
 TIME_ZONE = 'UTC'
 
@@ -146,3 +147,8 @@ LOGIN_URL='logi'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    locale.setlocale(locale.LC_ALL,'es_CO.UTF-8')
+except locale.Error:
+    pass    
