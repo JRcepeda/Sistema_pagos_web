e901b28a6961c321ce3874f3cabb89e29c3ab800
unknown <josercepeda01@gmail.com>
2024-03-17 16:57:47 -0500

    db render consult

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,10 @@
-from django.db import models,connection
+from django.db import models,connection,transaction
 
 
 # Create your models here tablas.
 
 #ejecuta proceso de pago creado en postgresql
-def consult_sql(documento):
+'''def consult_sql(documento):
     with connection.cursor() as cursor:
         
         sql=f"CALL PAGO('{documento}',{7600})"
@@ -16,6 +16,31 @@ def consult_sql(documento):
         saldo=cursor.fetchall()
         cursor.connection.commit()
     return notice,saldo
+'''
+def consult_sql(documento):
+    with connection.cursor() as cursor:
+        try:
+            with transaction.atomic():
+                sql1 = f"CALL PAGO('{documento}', {7600})"
+                cursor.execute(sql1)
+                
+                # Recupera los avisos generados por la llamada al procedimiento almacenado
+                notices = cursor.connection.notices
+                
+                sql2 = f"SELECT SALDO FROM SALDO_ESTUDIANTE WHERE CEDULA='{documento}'"
+                cursor.execute(sql2)
+                
+                saldo = cursor.fetchall()
+                
+                # Confirma la transacción
+                transaction.commit()
+                
+                # Devuelve los avisos y el saldo
+                return notices, saldo
+        except Exception as e:
+            # Si hay algún error, realiza un rollback de la transacción
+            transaction.rollback()
+            raise e
 
 def consult_sql2(documento):
     with connection.cursor() as cursor:
