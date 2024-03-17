ae690ab220e49e79dc20fa621141d779e58f07c4
unknown <josercepeda01@gmail.com>
2024-03-17 17:29:24 -0500

    if sql consult2

--- tasks/views.py
+++ tasks/views.py
@@ -18,13 +18,15 @@ def datos_sql(request):
 #        try:       
         notice,saldo=consult_sql(request.POST['documento'])
         locale.setlocale(locale.LC_ALL,'es_CO.UTF-8')
-        if saldo[0][0]>=0:
-            saldo=[locale.currency(saldo[0][0],grouping=True)]
-            return render(request,'consultas.html',{'notice':notice,'saldo':saldo})
- #       except:
-        else:
+        
+        try:
+            if saldo[0][0]>=0:
+                saldo=[locale.currency(saldo[0][0],grouping=True)]
+                return render(request,'consultas.html',{'notice':notice,'saldo':saldo})
+    #       except:
+        except:
             return render(request,'consultas.html',{'notice':['Digite un numero de documento'],'saldo':[0]})
-      #      return render(request,'consultas.html',{'notice':notice,'saldo':saldo})
+        #      return render(request,'consultas.html',{'notice':notice,'saldo':saldo})
 
 #peticion de saldo
 
